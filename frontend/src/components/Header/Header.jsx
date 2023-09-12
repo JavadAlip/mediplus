af5c9cb1fb200c29352f421c54c3b9438b4135e1
JavadAlip <122251720+JavadAlip@users.noreply.github.com>
2023-09-13 01:30:44 +0530

    header set

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -24,6 +24,24 @@ let navLinks = [
 ]
 
 const Header = () => {
+  const headerRef=useRef(null)
+  const menuRef=useRef(null)
+  const handleStickyHeader=()=>{
+    window.addEventListener('scroll',()=>{
+      if(document.body.scrollTop>80 || document.documentElement.scrollTop>80){
+        headerRef.current.classList.add('sticky__header')
+        
+      }else{
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+  useEffect(()=>{
+    handleStickyHeader()
+    return()=>window.removeEventListener('scroll',handleStickyHeader)
+  })
+  const toggleMenu=()=> menuRef.current.classList.toggle('show__menu')
+
   return (
     <header className="header flex items-center">
       <div className="container">
@@ -35,7 +53,7 @@ const Header = () => {
           </div>
 
           {/* menu */}
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu flex items-center gap-[2.7rem] ">
               {
                 navLinks.map((link, index) =>(
@@ -51,6 +69,7 @@ const Header = () => {
 
           {/* nav right */}
           <div className=''> 
+          <div className='hidden'>
             <Link to='/'>
               <figure className='w-[35px] h-[35px]  rounded-full cursor-pointer '>
                 <img src={userImg} className='w-full rounded-full' alt="" />
@@ -64,10 +83,11 @@ const Header = () => {
           </Link>
           </div>
 
-          <span className='md:hidden' >
+          <span className='md:hidden' onClick={toggleMenu} >
             <BiMenu className='w-6 h-6 cursor-pointer'/>
           </span>
 
+          </div>
         </div>
       </div>
     </header>
