43469d1c4d629bc5e0005c7c9d0a71d8d71ddbea
javadAlip <javadalipp911@gmail.com>
2023-09-15 16:37:10 -0700

    latest

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           <div className='lg:w-[570px]'>
             <h1 className='text-[36px] leading-[46px] text-primaryColor font-[800] md:text-[60px] md:leading-[70px]'>Your Healthy life is Our First Priority.</h1>
             <p className='text_para font-[500] '>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            <button className='btn'>Request an Appoinmentt</button>
+            <button className='btn'>Request an Appoinment</button>
           </div>
         </div>
 
