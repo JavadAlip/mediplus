56fdf7fe012c0df2bc4877c7f464a405877e2b9d
javadAlip <javadalipp911@gmail.com>
2023-09-16 14:13:57 +0530

    services

--- frontend/src/components/Services/SercviceCard.jsx
+++ frontend/src/components/Services/SercviceCard.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ServiceCard = ({item,index}) => {
 const {name,desc,bgColor, textColor}=item
    return<div className="py-[30px] px-3 lg:px-5 ">
     <h2 className='text-[26px] leading-9 text-primaryColor font-[700]'>{name}</h2>
     <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>{desc}</p>
-    <div className='flex '>
+    <div className='flex item-center justify-between mt-[30px]'>
+        <Link
+        to=
+        >
+
+        
+        </Link>
 
     </div>
    </div>
