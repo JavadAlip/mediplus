55a08e30f272fab2f03871d3a104b47acbffed1b
javadAlip <javadalipp911@gmail.com>
2023-09-17 23:55:22 +0530

    new

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -60,7 +60,7 @@ const Home = () => {
       <div className='xl:w-[470px] mx-auto'>
           <h2 className='heading text-center'>What our patients say</h2>
           <p className='text__para text-center text-textColor'>world-class care for everyone. Our health System offers unmatched,
-            expert health care.</p>
+            expert health care</p>
         </div>
 
       <Testsmonial/>
