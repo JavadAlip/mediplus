fbacbb276585bd8a6cafc00425dacb86641541bb
javadAlip <javadalipp911@gmail.com>
2023-09-18 23:53:51 +0530

    new

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {doctors} from '../../assets/data/doctor'
+import DoctorCard from'../../components/Doctors/DoctorCard'
 
 const Doctors = () => {
-  return (
-    <div>Doctors</div>
-  )
+  return <>
+  <section className='bg-[#fff9ea]'>
+    <div className='container text-center'>
+      <h2 className='heading'>Find a Doctor</h2>
+    </div>
+  </section>
+  
+  </>
+    
+  
 }
 
 export default Doctors
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { services } from '../assets/data/service'
+import ServiceCard from '../components/Services/SercviceCard'
 
 const Services = () => {
-  return (
-    <div>Services</div>
-  )
+  return <section>
+    <div className="container">
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px]'>
+        {services.map((item, index) => (
+          <ServiceCard item={item} index={index} key={index} />))}
+      </div>
+    </div>
+  </section>
+
+
 }
 
 export default Services
