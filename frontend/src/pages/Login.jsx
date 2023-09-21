290c5241613c210e3fda4db6b0419586e528c090
javadAlip <javadalipp911@gmail.com>
2023-09-22 01:19:10 +0530

    littleChangeInLoginPage

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Login = () => {
   return <section className='px-5 lg:px-0'>
     <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
-      <h3 className='text-primaryColor text-[22px] leading-7 font-bold mb-10'>Hello <span
-      className='text-greenColor'>Welcome</span> Back</h3>
+      <h3 className='text-primaryColor text-[22px] leading-7 font-bold mb-10'>Please <span
+      className='text-greenColor'> Login!</span></h3>
     </div>
 
   </section>
