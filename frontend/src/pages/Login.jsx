fa485bf6237a2b3433d63fae3d31a958e1dc1aac
javadAlip <javadalipp911@gmail.com>
2023-09-22 01:42:52 +0530

    loginPageSetting

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
-  return <section className='px-5 lg:px-0'>
-    <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
-      <h3 className='text-primaryColor text-[22px] leading-7 font-bold mb-10'>Please <span
-      className='text-greenColor'> Login!</span></h3>
-    </div>
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
 
-  </section>
-   
-  
-}
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-export default Login
+  return (
+    <section className='px-5 lg:px-0'>
+      <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
+        <h3 className='text-primaryColor text-[22px] leading-7 font-bold mb-10'>Please <span
+          className='text-greenColor'> Login!</span></h3>
+        <form className='py-4 md:py-0'>
+          <div className='mb-5'>
+            <input
+              type='email'
+              placeholder='Enter Your Email'
+              name='email'
+              value={formData.email}
+              onChange={handleInputChange}
+              className='w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none
+              focus:border-b-primaryColor text-[22px] leading-7 text-textColor placeholder:text-textColor rounded-md cursor-pointer'
+            />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
