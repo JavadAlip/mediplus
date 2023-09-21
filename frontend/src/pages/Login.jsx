940e64c9f8afbfb07f1a222bece886d46d67d75e
javadAlip <javadalipp911@gmail.com>
2023-09-22 01:14:09 +0530

    LoginStart

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  return <section className='px-5 lg:px-0'>
+    <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
+      <h3 className='text-primaryColor text-[22px] leading-7 font-bold mb-10'>Hello <span
+      className='text-greenColor'>Welcome</span> Back</h3>
+    </div>
+
+  </section>
+   
+  
 }
 
 export default Login
