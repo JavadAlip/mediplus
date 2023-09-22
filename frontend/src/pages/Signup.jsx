6080d7e8b7095c4bd390829328cbe6842d375e6e
javadAlip <javadalipp911@gmail.com>
2023-09-22 21:19:23 +0530

    signupStart

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import signupImg from "../assets/images/signup.gif"
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <section className='px-5 xl:px-0'>
+      <div className='max-w-[1170px] mx-auto'>
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+          {/* img box */}
+          <div className='hidden lg:block bg-primaryColor rounded-l-lg'>
+            <figure className='rounded-l-lg'>
+              <img src={signupImg} alt="" />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default Signup
+export default Signup;
