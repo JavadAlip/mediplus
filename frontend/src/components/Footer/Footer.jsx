1e2b7c5485c0200f83367dc41fa8ea76c0a585ed
javadAlip <javadalipp911@gmail.com>
2023-09-23 14:28:00 +0530

    FooterAddGithubLink

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className='pb-16 pt-10' >
+    <footer className='pb-10 pt-5' >
     <div className='container'>
       <div className='flex justify-between flex-col flex-wrap gap-[30px]' >
         <div>
         
-       <p className='text-[16px] leading-7 font-[400] text-textColor text-center' >Copyright @2023 developed by JAVAD all right reserved.</p>
+        <p class='text-[16px] leading-7 font-[400] text-textColor text-center'>
+  © 2023 Designed by <a href="https://github.com/JavadAlip" target="_blank" rel="noopener noreferrer">Javad</a>. All rights reserved.
+</p>
+
         </div>
       </div>
     </div>
