47b95c5987aac9897f41f824a231ad00cac5994c
javadAlip <javadalipp911@gmail.com>
2023-09-24 00:06:15 +0530

    DoctorPageSizeAdjst

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -16,8 +16,9 @@ const DoctorCard = ({doctor}) => {
             <span className='bg-[#ccf0f3] text-primaryColor py-1 px-2 lg:py-2 lg:px-6 text-[18px]
             leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded'>{specialty}</span>
             <div className='flex item-center gap-[6px]'>
-                <span className='flex item-center gap-[6px] text-[14px] leading-6 lg:text-[16px] lg:leading-7
-                font-semibold text-primaryColor'><img src={starIcon} alt="" style={{height:23}}/>{avgRating}</span>
+                <span className='flex item-center gap-[5px] text-[14px] leading-6 lg:text-[16px] lg:leading-7
+                font-semibold text-primaryColor'><img src={starIcon} alt="" style={{height:21}}/>{avgRating}</span>
+
                 <span className='text-[14px] leading-6 lg:text-[16px] lg:leading-7
                 font-[400] text-textColor'>({totalRating})</span>
             </div>
--- frontend/src/components/Doctors/DoctorList.jsx
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -3,9 +3,10 @@ import {doctors} from '../../assets/data/doctor';
 import DoctorCard from './DoctorCard'
 
 const DoctorList = () => {
-    return (
-        <div className=" text-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px]
+    return ( <div className='container'>
+        <div className=" text-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[0px] mt-[30px]
         ">{doctors.map((doctor)=><DoctorCard key={doctor.id} doctor={doctor}/>)}</div>
+        </div>
       )
 }
 
