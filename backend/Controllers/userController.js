5576863474a765a31c2ee758aba1cf10a170f6a5
javadAlip <javadalipp911@gmail.com>
2023-09-26 20:41:30 +0530

    user-controller-complete

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,55 @@
+import User from "../models/UserSchema.js";
+
+//Update user profile
+export const updateUser = async(req,res)=>{
+    const id = req.params.id
+
+    try {
+        const updateUser = await User.findByIdAndUpdate(id, {$set:req.body}, {new:true})
+        res.status(200).json({success:true,message:"Successfully Updated", data:updateUser})
+        
+    } catch (error) {
+        res.status(500).json({success:false,message:"Failed to Update", data:updateUser})
+        
+    }
+}
+//Delete user profile
+export const deleteUser = async(req,res)=>{
+    const id = req.params.id
+
+    try {
+        const deleteUser = await User.findByIdAndDelete(id, {$set:req.body}, {new:true})
+        res.status(200).json({success:true,message:"Successfully deleted", data:deleteUser})
+        
+    } catch (error) {
+        res.status(500).json({success:false,message:"Failed to delete"})
+        
+    }
+}
+
+//get a single user
+export const getSingleUser = async(req,res)=>{
+    const id = req.params.id
+
+    try {
+        const user = await User.findById(id)
+        res.status(200).json({success:true,message:"User found", data:user})
+        
+    } catch (error) {
+        res.status(404).json({success:false,message:"No user found", data:updateUser})
+        
+    }
+}
+//get all user
+export const getAllUser = async(req,res)=>{
+   
+
+    try {
+        const users = await User.find({})
+        res.status(200).json({success:true,message:"Users found", data:users})
+        
+    } catch (error) {
+        res.status(404).json({success:false,message:"Not found"})
+        
+    }
+}
