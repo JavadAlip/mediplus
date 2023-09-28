288c101bc1953260a8828c7da7607bfe7bbd53b1
javadAlip <javadalipp911@gmail.com>
2023-09-28 21:18:43 +0530

    user-doctor.js-add-restiction-with-authentication

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { updateDoctor, deleteDoctor, getSingleDoctor, getAllDoctor } from "../Controllers/doctorController.js";
+import {authenticate, restrict} from "../auth/verifyToken.js"
 
 const router = express.Router()
 router.get('/:id', getSingleDoctor)
 router.get('/', getAllDoctor)
-router.put('/:id', updateDoctor)
-router.delete('/:id', deleteDoctor)
+router.put('/:id',authenticate, restrict(["doctor"]), updateDoctor)
+router.delete('/:id',authenticate, restrict(["doctor"]), deleteDoctor)
 
 export default router;
--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { updateUser, deleteUser, getAllUser,getSingleUser } from "../Controllers/userController.js";
 
-import {authenticate} from "../auth/verifyToken.js"
+import {authenticate, restrict} from "../auth/verifyToken.js"
 const router = express.Router()
-router.get('/:id',authenticate, getSingleUser)
-router.get('/', getAllUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
+router.get('/:id',authenticate, restrict(["patient"]), getSingleUser)
+router.get('/', authenticate, restrict(["admin"], getAllUser))
+router.put('/:id',authenticate, restrict(["patient"]), updateUser)
+router.delete('/:id',authenticate, restrict(["patient"]), deleteUser)
 
 export default router;
