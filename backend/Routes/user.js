3c8256a0139d07233d672237ff10dbf663c67641
javadAlip <javadalipp911@gmail.com>
2023-09-28 20:38:02 +0530

    add-a-change

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { updateUser, deleteUser, getAllUser,getSingleUser } from "../Controllers/userController.js";
 
+import {authenticate} from "../auth/verifyToken.js"
 const router = express.Router()
-router.get('/:id', getSingleUser)
+router.get('/:id',authenticate, getSingleUser)
 router.get('/', getAllUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
