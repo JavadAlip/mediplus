4df86600636561f6fd8dacb41ffb7cfaebdc4be0
javadAlip <javadalipp911@gmail.com>
2023-09-28 20:46:02 +0530

    Token-authentication-almost-complete

---
+++ backend/auth/verifyToken.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+import Doctor from '../models/DoctorSchema.js'
+import User from '../models/UserSchema.js'
+
+export const authenticate = async (req,res,next)=>{
+
+    //get token from headers
+    const authToken = req.headers.authorization;
+
+    //check token is exists
+    if(!authToken || !authToken.startsWith('Bearer')){
+        return res.status(401).json({success:false, message:"No token, authorization denied"})
+    }
+    try {
+        const token = authToken.split(" ")[1];
+
+        //verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+
+        req.userId = decoded.id
+        req.role = decoded.role
+
+        next();
+    } catch (error) {
+        if(error.name =='TokenExpiredError'){
+            return res.status(401).json({message:'Token is expired'})
+        }
+        return res.status(401).json({success:false, message:"Invalid token"})
+        
+    }
+        
+}
