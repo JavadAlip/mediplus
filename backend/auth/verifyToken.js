2d3f9df002263f0a8eb1269c5b2287be5558418e
javadAlip <javadalipp911@gmail.com>
2023-09-28 21:17:15 +0530

    Restriction-add-itMeans-adminCan't-updateUser.

--- backend/auth/verifyToken.js
+++ backend/auth/verifyToken.js
@@ -25,8 +25,27 @@ export const authenticate = async (req,res,next)=>{
         if(error.name =='TokenExpiredError'){
             return res.status(401).json({message:'Token is expired'})
         }
-        return res.status(401).json({success:false, message:"Invalid token"})
-        
+        return res.status(401).json({success:false, message:"Invalid token"}) 
     }
-        
+};
+
+//the user doctor or patient is desiding 
+
+export const restrict = roles => async (req,res,next)=>{
+    const userId = req.userId
+
+    let user;
+    const patient = await User.findById(userId)
+    const doctor = await Doctor.findById(userId)
+    if(patient){
+        user = patient
+    }
+    if(doctor){
+        user = doctor
+    }
+    if(!roles.includes(user.role)){
+        return res.status(401).json({success:false, message:"You're not authorized"})
+    }
+
+    next();
 }
