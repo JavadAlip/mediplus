fb16992f5dfd8bd256f6416624d05f9f33d61dcf
javadAlip <javadalipp911@gmail.com>
2023-10-06 22:57:35 +0530

    service-content-update

--- frontend/src/assets/data/service.js
+++ frontend/src/assets/data/service.js
@@ -1,33 +1,34 @@
 export const services = [
     {
       name: "Cancer Care",
-      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      desc: "Experience top-tier cancer care: Our team provides compassionate support, advanced treatments, and personalized care to guide you towards wellness and recovery.",
+      bgColor: "rgba(254, 182, 13, .2)",
+      textColor: "#FEB60D",
+    },
+   
+    {
+      name: "Neurology",
+      desc: "Elevate your neurology care: Our expert specialists, advanced diagnostics, and compassionate treatment prioritize your neurological health and enhance your quality of life.",
       bgColor: "rgba(254, 182, 13, .2)",
       textColor: "#FEB60D",
     },
     {
-      name: "Labor & Delivery",
-      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
-      bgColor: "rgba(151, 113, 255, .2)",
-      textColor: "#9771FF",
-    },
-    {
       name: "Heart & Vascular",
-      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      desc: "Exceptional heart and vascular care: Expert specialists, advanced treatments, and compassionate support for your cardiovascular well-being.",
       bgColor: "rgba(1, 181, 197, .2)",
       textColor: "#01B5C5",
     },
     {
       name: "Mental Health",
-      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      desc: "Discover excellence in mental health care: Our skilled professionals offer compassionate support and personalized treatment to enhance your well-being.",
       bgColor: "rgba(1, 181, 197, .2)",
       textColor: "#01B5C5",
     },
     {
-      name: "Neurology",
-      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
-      bgColor: "rgba(254, 182, 13, .2)",
-      textColor: "#FEB60D",
+      name: "Labor & Delivery",
+      desc: "Experience exceptional labor and delivery care with skilled professionals in a safe environment, providing personalized support for life's most precious moments.",
+      bgColor: "rgba(151, 113, 255, .2)",
+      textColor: "#9771FF",
     },
     {
       name: "Burn Treatment",
