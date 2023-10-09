1949ee64415a8fd582ddce9def7a7a3cd0a11987
javadAlip <javadalipp911@gmail.com>
2023-10-10 01:29:00 +0530

    user login depended take action

---
+++ frontend/src/routers/ProtectRoute.jsx
@@ -0,0 +1,13 @@
+import {useContext} from 'react'
+import { Navigate } from 'react-router-dom'
+import { authContext } from '../context/authContext'
+
+export const ProtectRoute = ({children, allowedRoles}) => {
+
+    const {token, role} = useContext(authContext)
+    const isAllowed = allowedRoles.includes(role)
+    const accessibleRoute = token && isAllowed ? children : <Navigate to='/login' replace={true}/>
+
+  return accessibleRoute;
+}
+export default ProtectRoute;
