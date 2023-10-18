4473df206b409ed83657d444e375f08c30117414
javadAlip <javadalipp911@gmail.com>
2023-10-18 23:20:13 +0530

    Socket.io backend connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,55 @@
-import express  from "express";
+// import express from "express";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import authRoute from "./Routes/Auth.js";
+// import userRoute from "./Routes/user.js";
+// import doctorRoute from "./Routes/doctor.js";
+// import reviewRoute from "./Routes/review.js";
+
+// dotenv.config();
+// const app = express();
+// const port = process.env.PORT || 8000;
+// const corsOptions = {
+//   origin: true,
+// };
+// app.get("/", (req, res) => {
+//   res.send("API is working");
+// });
+
+// // Database connection
+// mongoose.set('strictQuery', false);
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB database is connected');
+//   } catch (err) {
+//     console.log('MongoDB database connection failed');
+//   }
+// };
+
+// // Middleware
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors(corsOptions));
+// app.use('/api/v1/Auth', authRoute);
+// app.use('/api/v1/users', userRoute);
+// app.use('/api/v1/doctors', doctorRoute);
+// app.use('/api/v1/reviews', reviewRoute); // Fixed the path
+
+// app.listen(port, () => {
+//   connectDB();
+//   console.log('Server is running on port ' + port);
+// });
+
+//---------------------------------------------------------------------//
+import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoute from "./Routes/Auth.js";
@@ -8,45 +57,64 @@ import userRoute from "./Routes/user.js";
 import doctorRoute from "./Routes/doctor.js";
 import reviewRoute from "./Routes/review.js";
 
+import http from "http";
+import { Server } from "socket.io";
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
 
-dotenv.config()
-const app= express()
-const port=process.env.PORT || 8000
-const corsOptions={
-    origin:true,
-};
-app.get('/',(req,res)=>{
-    res.send("Api is working")
-})
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
 
+    socket.on("disconnect",()=> console.log("User disconnected"))
+});
 
-//database connection
-mongoose.set('strictQuery',false)
-const connectDB=async()=>{
+// \--------------------------------------\
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    res.send("API is working");
+});
+
+// Database connection
+const connectDB = async () => {
     try {
-       await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser : true,
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
             useUnifiedTopology: true,
-        })
-        console.log('mongoDB database is connected')
+        });
+        console.log('MongoDB database is connected');
     } catch (err) {
-        console.log('mongoDB database is connection failed') 
+        console.error('MongoDB database connection failed', err);
     }
 }
 
-//middleware
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors(corsOptions))
-app.use('/api/v1/Auth',authRoute)
-app.use('/api/v1/users',userRoute)
-app.use('/api/v1/doctors',doctorRoute)
-app.use('api/v1/reviews',reviewRoute)
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
 
+// Define CORS options
+const corsOptions = {
+    origin: 'http://localhost:5173', // Change to your actual frontend origin
+    methods: ['GET', 'POST'],
+};
 
-app.listen(port, ()=>{
+app.use(cors(corsOptions));
+
+// Define routes
+app.use('/api/v1/Auth', authRoute);
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/doctors', doctorRoute);
+app.use('/api/v1/reviews', reviewRoute);
+
+app.listen(port, () => {
     connectDB();
-    console.log('Server is running on port'+ port)
-})
-
-
+    console.log('Server is running on port ' + port);
+});
