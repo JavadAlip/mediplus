e64e5bdcb71e8a079ec953e1120fde677d9a0309
javadAlip <javadalipp911@gmail.com>
2023-11-10 21:18:37 +0530

    code alighned and remove spaces

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,3 @@
-// import express from "express";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import authRoute from "./Routes/Auth.js";
-// import userRoute from "./Routes/user.js";
-// import doctorRoute from "./Routes/doctor.js";
-// import reviewRoute from "./Routes/review.js";
-
-// dotenv.config();
-// const app = express();
-// const port = process.env.PORT || 8000;
-// const corsOptions = {
-//   origin: true,
-// };
-// app.get("/", (req, res) => {
-//   res.send("API is working");
-// });
-
-// // Database connection
-// mongoose.set('strictQuery', false);
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('MongoDB database is connected');
-//   } catch (err) {
-//     console.log('MongoDB database connection failed');
-//   }
-// };
-
-// // Middleware
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(cors(corsOptions));
-// app.use('/api/v1/Auth', authRoute);
-// app.use('/api/v1/users', userRoute);
-// app.use('/api/v1/doctors', doctorRoute);
-// app.use('/api/v1/reviews', reviewRoute); // Fixed the path
-
-// app.listen(port, () => {
-//   connectDB();
-//   console.log('Server is running on port ' + port);
-// });
-
-//---------------------------------------------------------------------//
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -72,8 +23,17 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
 
-    socket.on("disconnect",()=> console.log("User disconnected"))
+    socket.on("send-message", (message) => {
+        console.log(message);
+
+        //Brodcast the recieved message to all connected users
+        io.emit("received-message", message)
+    })
+
+    socket.on("disconnect", () => console.log("User disconnected"))
 });
+
+server.listen(3000, () => console.log("server running at port 3000"))
 
 // \--------------------------------------\
 dotenv.config();
@@ -102,7 +62,7 @@ app.use(cookieParser());
 
 // Define CORS options
 const corsOptions = {
-    origin: 'http://localhost:5173', // Change to your actual frontend origin
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
 };
 
