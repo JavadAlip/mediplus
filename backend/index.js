a284a77549c73f8b17d28941646f855bf032a0be
JavadAlip <javadalipp911@gmail.com>
2024-02-15 01:01:21 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ import userRoute from "./Routes/user.js";
 import doctorRoute from "./Routes/doctor.js";
 import reviewRoute from "./Routes/review.js";
 
+import bookingRoute from "./Routes/booking.js"
+
 import http from "http";
 import { Server } from "socket.io";
 const app = express();
@@ -72,6 +74,7 @@ app.use('/api/v1/Auth', authRoute);
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/doctors', doctorRoute);
 app.use('/api/v1/reviews', reviewRoute);
+app.use('/api/v1/bookings', bookingRoute);
 
 app.listen(port, () => {
     connectDB();
