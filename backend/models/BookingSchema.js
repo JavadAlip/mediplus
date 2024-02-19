c302b6e5047047317654fa10e9e6315a108c237d
JavadAlip <javadalipp911@gmail.com>
2024-02-19 20:09:45 +0530

    Models update

--- backend/models/BookingSchema.js
+++ backend/models/BookingSchema.js
@@ -13,10 +13,10 @@ const bookingSchema = new mongoose.Schema(
       required: true,
     },
     ticketPrice: { type: String, required: true },
-    appointmentDate: {
-      type: Date,
-      required: true,
-    },
+    // appointmentDate: {
+    //   type: Date,
+    //   required: true,
+    // },
     status: {
       type: String,
       enum: ["pending", "approved", "cancelled"],
@@ -29,5 +29,12 @@ const bookingSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user').populate('doctor');
+  next();
+});
+
+
 
 export default mongoose.model("Booking", bookingSchema);
--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -1,3 +1,32 @@
+// import mongoose from "mongoose";
+
+// const UserSchema = new mongoose.Schema({
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   name: { type: String, required: true },
+//   phone: { type: Number },
+//   photo: { type: String },
+//   role: {
+//     type: String,
+//     enum: ["patient", "admin"],
+//     default: "patient",
+//   },
+//   gender: { type: String, enum: ["male", "female", "other"] },
+//   bloodType: { type: String },
+//   appointments: [{ type: mongoose.Types.ObjectId, ref: "Appointment" }],
+//   address: {
+//     line1: { type: String },
+//     line2: { type: String },
+//     city: { type: String },
+//     postal_code: { type: String },
+//     country: { type: String },
+//   },
+// });
+
+// export default mongoose.model("User", UserSchema);
+
+
+
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
@@ -15,4 +44,5 @@ const UserSchema = new mongoose.Schema({
   bloodType: { type: String },
   appointments: [{ type: mongoose.Types.ObjectId, ref: "Appointment" }],
 });
+
 export default mongoose.model("User", UserSchema);
