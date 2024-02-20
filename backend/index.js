c2a828ba9505b8c0b052a17628f69d577a7abc50
JavadAlip <javadalipp911@gmail.com>
2024-02-20 23:24:14 +0530

    local frontend url change

--- backend/index.js
+++ backend/index.js
@@ -65,7 +65,7 @@ app.use(cookieParser());
 
 // Define CORS options
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://mediplus-wyry.vercel.app',
     methods: ['GET', 'POST'],
 };
 
