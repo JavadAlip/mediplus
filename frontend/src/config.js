ce439cea75d7c2baa253cfc50ffd449669e3fe5b
JavadAlip <javadalipp911@gmail.com>
2024-02-20 23:18:31 +0530

    frontend and backend url changed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://mediplus-92z5.onrender.com';
 export const token = localStorage.getItem('token')
--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import io from "socket.io-client"
-const socket = io("http://localhost:3000")
+const socket = io("https://mediplus-92z5.onrender.com")
 const Chat = () => {
   const [username, setUserame] = useState("")
   const [ChatActive, setChatActive] = useState(false)
