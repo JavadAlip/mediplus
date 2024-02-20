209490e983b3bd46527a4af470d8799540874cfd
JavadAlip <javadalipp911@gmail.com>
2024-02-20 23:00:39 +0530

    star to Star

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import DocImg from '../../assets/images/doctor-img01.png';
-import starIcon from '../../assets/images/star.png';
+import starIcon from '../../assets/images/Star.png';
 import DoctorAbout from './DoctorAbout';
 import Feedback from './Feedback';
 import SidePanel from './SidePanel';
