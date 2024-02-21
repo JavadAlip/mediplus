a78d441765f200669fad470da38b3687ba10d5ce
JavadAlip <javadalipp911@gmail.com>
2024-02-21 13:46:51 +0530

    vite.config file modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
 })
